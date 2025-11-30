bf757ce647e8b8bb1f8e6ed3e22399d40d6b56b5
piotrrus <irache@wp.pl>
2025-11-30 23:29:53 +0100

    Add files via upload

---
+++ src/app/shared/models/date-formats.ts
@@ -0,0 +1,13 @@
+// import { NgxMatDateFormats } from '@angular-material-components/datetime-picker';
+
+export const DATE_FORMAT_PL: any = {
+     parse: {
+          dateInput: 'YYYY-MM-DD, HH:mm',
+     },
+     display: {
+          dateInput: 'YYYY-MM-DD',
+          monthYearLabel: 'MMMM YYYY',
+          dateA11yLabel: 'LL',
+          monthYearA11yLabel: 'MMMM YYYY',
+     },
+};
